c298f0057aa7c42310eec8b8d106fc0765469bce
xequemate28 <xequemate28@hotmail.com>
2025-08-20 21:15:19 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+a = int(input("digite um número:"))
+b = int(input("digite o outro número:"))
+
+#operaçoões
+soma = a + b
+subtração = a - b 
+divisão = a / b 
+multiplicação = a * b 
+
+ #resultado
+print("soma",soma)
+print("subtração",subtração)
+print("divisão",divisão)
+print("multiplicação",multiplicação)
