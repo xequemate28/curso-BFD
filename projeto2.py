c192775240e7823885b355a56d64a36991f49678
xequemate28 <xequemate28@hotmail.com>
2025-08-25 21:12:18 -0300

    Add files via upload

---
+++ projeto2.py
@@ -0,0 +1,12 @@
+mensagem_do_dia = int(input("Digite um número da sorte( 1 a 3):"))
+
+if mensagem_do_dia == 1:
+    print("Não se preocupe com o futuro, ele já está rindo de voçê!")
+elif mensagem_do_dia == 2:
+    print("Hoje ninguém vai te pedir dinheiro emprestado!")
+elif mensagem_do_dia == 3:
+    print("Seu dia será próspero!")
+else:
+    print("Voçê digitou o número errado!")
+
+
